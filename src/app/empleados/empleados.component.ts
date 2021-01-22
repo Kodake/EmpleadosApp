c23a08a604a87a86b42747cba7898bf425b87882
Kodake <39713700+Kodake@users.noreply.github.com>
2021-01-22 03:59:50 -0400

    Adding Pagination to EmpleadosApp

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -8,18 +8,10 @@ import { EmpleadoService } from '../services/empleado.service';
   styleUrls: ['./empleados.component.css']
 })
 export class EmpleadosComponent implements OnInit {
+
   generoSelec = 'Todos';
   listEmpleados: Empleado[] = [];
-  // listEmpleados: Empleado[] = [
-  //   { legajo: 1, nombre: 'Rudy', apellido: 'Florentino', genero: 'Masculino', salario: 100000},
-  //   { legajo: 2, nombre: 'Juan', apellido: 'Reyes', genero: 'Masculino', salario: 110000},
-  //   { legajo: 3, nombre: 'Aarón', apellido: 'Florentino', genero: 'Masculino', salario: 95000},
-  //   { legajo: 4, nombre: 'Rosa', apellido: 'Castro', genero: 'Femenino', salario: 80000},
-  //   { legajo: 5, nombre: 'Alicia', apellido: 'Olivares', genero: 'Femenino', salario: 75000},
-  //   { legajo: 6, nombre: 'María', apellido: 'Méndez', genero: 'Femenino', salario: 60000},
-  //   { legajo: 7, nombre: 'Rafaela', apellido: 'Menéndez', genero: 'Femenino', salario: 55000},
-  //   { legajo: 8, nombre: 'Pedro', apellido: 'Castaños', genero: 'Masculino', salario: 55000}
-  // ];
+  public pageNumber: number = 1;
 
   constructor(private empleadoService: EmpleadoService) { }
 
