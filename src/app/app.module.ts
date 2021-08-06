c9407029c5beeced5e4280f25ddbb79cbfc40365
Kodake <kodake@hotmail.es>
2021-08-06 16:55:33 -0400

    Changes about adding ngx-charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { NgxPaginationModule } from 'ngx-pagination';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { CountEmpleadosComponent } from './empleados/count-empleados/count-empleados.component';
+import { ChartsComponent } from './empleados/charts/charts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadosComponent,
-    CountEmpleadosComponent
+    CountEmpleadosComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    NgxChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
